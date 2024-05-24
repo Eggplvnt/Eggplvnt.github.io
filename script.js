0ea14beadefcab9007054135cc917d5d1c144d42
Eggplvnt <113505947+3ggplant@users.noreply.github.com>
2024-05-23 18:29:39 -0700

    update js

--- script.js
+++ script.js
@@ -1,47 +1,28 @@
 
-function showModal(modalId) {
-    var modal = document.getElementById(modalId);
-    modal.style.display = "block";
-}
+document.addEventListener('DOMContentLoaded', function () {
+    var modals = document.querySelectorAll('.modal');
+    var openModalBtns = document.querySelectorAll('[id^=openModalBtn]');
+    var closeModalBtns = document.querySelectorAll('.close');
 
-function hideModal(modalId) {
-    var modal = document.getElementById(modalId);
-    modal.style.display = "none";
-}
-
-document.getElementById("openModalBtn1").addEventListener("click", function() {
-    showModal("myModal1");
-});
-
-document.getElementById("openModalBtn2").addEventListener("click", function() {
-    showModal("myModal2");
-});
-
-document.getElementById("openModalBtn3").addEventListener("click", function() {
-    showModal("myModal3");
-});
-
-document.getElementById("openModalBtn4").addEventListener("click", function() {
-    showModal("myModal4");
-});
-
-document.getElementById("openModalBtn5").addEventListener("click", function() {
-    showModal("myModal5");
-});
-
-var closeButtons = document.getElementsByClassName("close");
-for (var i = 0; i < closeButtons.length; i++) {
-    closeButtons[i].addEventListener("click", function() {
-        var modalId = this.parentNode.parentNode.id;
-        hideModal(modalId);
+    openModalBtns.forEach(function (btn) {
+        btn.addEventListener('click', function () {
+            var modalId = this.id.replace('openModalBtn', 'myModal');
+            var modal = document.getElementById(modalId);
+            modal.style.display = 'flex';
+        });
     });
-}
 
-window.addEventListener("click", function(event) {
-    var modals = document.getElementsByClassName("modal");
-    for (var i = 0; i < modals.length; i++) {
-        if (event.target == modals[i]) {
-            hideModal(modals[i].id);
-        }
-    }
+    closeModalBtns.forEach(function (btn) {
+        btn.addEventListener('click', function () {
+            this.closest('.modal').style.display = 'none';
+        });
+    });
+
+    window.addEventListener('click', function (event) {
+        modals.forEach(function (modal) {
+            if (event.target == modal) {
+                modal.style.display = 'none';
+            }
+        });
+    });
 });
